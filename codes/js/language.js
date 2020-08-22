0bc720612d4d88c005975d535475735f78b79206
chldnwls0712 <wjc0712h@gmail.com>
2020-08-23 00:15:49 +0900

    drapNdrop and languge

--- codes/js/language.js
+++ codes/js/language.js
@@ -1,28 +1,43 @@
 var language = {
-    eng: {
-        explain:"free online tool converts <b>PNG to JPEG</b>, and <b>VIDEO to AUDIO.</b><br/>  this service does not ask for any personal information."
-    },
     kr: {
         explain:"간편하고 빠르게 원하는 이미지 파일 <b>PNG 에서 JPEG</b>, 그리고 <b>동영상 파일에서 오디오 파일</b><br/>로 바꾸어주는 서비스입니다. 이 서비스는 그 어떤 개인정보도 물어보지않습니다."
+    },
+    esp:{
+        explain:"La herramienta en línea gratuita convierte PNG a JPEG </b> y <b> VIDEO a AUDIO. </b> <br/> este servicio no solicita ninguna información personal."
     }
 };
 var filelangauge = {
-    eng: {
-        fileexplain: "Click to select the file."
-    },
     kr: {
-        fileexplain: "파일 선택을 위해 클릭하세요."
-    }
+        fileexplain: "파일을 업로드하세요."
+    },
+    kr1:{
+        fileexplain1: "(클릭 아니면 드래그&드랍)"
+    },
+    esp: {
+        fileexplain: "Sube el archivo."
+    },
+    esp1: {
+        fileexplain1: "(haga clic o arrastre y suelte)"
+    },
 };
-//define language based on the hash
+
+//switch language based on the hash
 if(window.location.hash)
 {
     if(window.location.hash == "#kor")
     {
         document.getElementById("description").innerHTML = language.kr.explain;
         document.getElementById("fileText").innerHTML = filelangauge.kr.fileexplain;
+        document.getElementById("fileText1").innerHTML = filelangauge.kr1.fileexplain1;
     } 
+    else if(window.location.hash == "#esp")
+    {
+        document.getElementById("description").innerHTML = language.esp.explain;
+        document.getElementById("fileText").innerHTML = filelangauge.esp.fileexplain;
+        document.getElementById("fileText1").innerHTML = filelangauge.esp1.fileexplain1;
+    }
 }
+//reload page
 function reloadFuc() {
     setTimeout(function () {
       location.reload();
--- codes/js/main.js
+++ codes/js/main.js
@@ -1,1 +1,24 @@
-//main.js
+let dropbox;
+dropbox = document.getElementById("dropbox");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+function dragenter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  
+  function dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  } 
+
+  function drop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  
+    const dt = e.dataTransfer;
+    const files = dt.files;
+  
+    handleFiles(files);
+}
